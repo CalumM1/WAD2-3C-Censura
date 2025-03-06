46707a85eec74e8ce9cbd9a810cc7720288cff41
Akuzike Komakoma <2465783k@student.gla.ac.uk>
2025-03-06 18:51:50 +0000

    Add Models skeleton

--- censura/models.py
+++ censura/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE);
+    
+class Movie(models.Model):
+    pass
+
+class Genre(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
+
+class Review(models.Model):
+    pass
