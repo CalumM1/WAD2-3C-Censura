8593604485ca6bb611792c137d016d0d1544b049
Calum McCarthy <2004mccarthy@gmail.com>
2025-03-06 17:45:23 +0000

    create views and urls

--- censura/urls.py
+++ censura/urls.py
@@ -6,5 +6,9 @@ app_name = 'censura'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
-    path('login/my_account/', views.my_account, name='my_account'),
+    path('signup/', views.signup, name='signup'),
+    path('about/', views.about, name='about'),
+    path('username/', views.my_account, name='my_account'),
+    path('username/my-favourites/', views.my_favourites, name='my_favourites'),
+    path('username/my-reviews/', views.my_reviews, name='my_reviews' ),
 ]
--- censura/views.py
+++ censura/views.py
@@ -9,3 +9,15 @@ def login(request):
 
 def my_account(request):
     return HttpResponse("My Account")
+
+def my_favourites(request):
+    return HttpResponse("My Favourites")
+
+def my_reviews(request):
+    return HttpResponse("My Reviews")
+
+def signup(request):
+    return HttpResponse("Sign Up")
+
+def about(request):
+    return HttpResponse("About")
