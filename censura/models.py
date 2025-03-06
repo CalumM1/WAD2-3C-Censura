644145329693e8d2ca5070df5024dd9d514e8a75
Akuzike Komakoma <2465783k@student.gla.ac.uk>
2025-03-06 19:03:58 +0000

    Implemment Movie model

--- censura/models.py
+++ censura/models.py
@@ -4,16 +4,25 @@ from django.template.defaultfilters import slugify
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE);
-    
-class Movie(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 
 class Genre(models.Model):
     pass
 
+
+class Movie(models.Model):
+    NAME_MAX_LENGTH = 50
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    director = models.CharField(max_length=NAME_MAX_LENGTH)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    #image = models.ImageField(upload_to=MEDIA_ROOT)
+    reviews = models.ManyToManyField()
+
+
 class Comment(models.Model):
     pass
+
 
 class Review(models.Model):
     pass
