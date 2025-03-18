dbc074725e353683ae94204dcabf101a2b17faad
Zike01 <zikeko170@gmail.com>
2025-03-18 21:51:22 +0000

    Implement login/logout, refactor edit_movies route

--- censura/views.py
+++ censura/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.urls import reverse
 from .forms import UserForm, UserProfileForm
 from .models import UserProfile
 
@@ -16,9 +18,25 @@ def index(request):
     return render(request, 'censura/index.html')
 
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('censura:my_account', args=[request.user.username]))
+        else:
+            return HttpResponse('invalid login details')
     return render(request, 'censura/login.html')
 
-def my_account(request):
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('censura:index'))
+
+def my_account(request, username):
     return render(request, 'censura/account.html')
 
 def my_favourites(request):
@@ -36,42 +54,39 @@ def signup(request):
             user.set_password(user.password)  # hash password
             user.save()
 
-            
             # create a UserProfile linked to this User
             UserProfile.objects.create(user=user)
             
-
             # Login the user
             login(request, user)
-            return redirect('censura:edit_profile')
-
+            return redirect(reverse('censura:edit_profile'))
     else:
         user_form = UserForm()
 
     return render(request, 'censura/signup.html', {'user_form': user_form})
 
-# @login_required
+@login_required
 def edit_profile(request):
     if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=request.user)
         profile_form = UserProfileForm(request.POST, instance=request.user.userprofile)
 
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
+        if profile_form.is_valid():
             profile_form.save()
-            return redirect('my_account')
+            return redirect(reverse('censura:my_account', args=[request.user.username]))
     else:
-        user_form = UserForm(instance=request.user)
         profile_form = UserProfileForm(instance=request.user.userprofile)
 
-    return render(request, 'censura/edit-profile.html', {'user_form': user_form, 'profile_form': profile_form})
+    return render(request, 'censura/edit-profile.html', {'profile_form': profile_form})
 
 def about(request):
     return render(request, 'censura/about.html')
 
 def view_movies(request):
     all_movies = Movie.objects.all()
-    context_dict = {"movies" : all_movies}
+    paginator = Paginator(all_movies, 24)
+    page = request.GET.get('page')
+    movies = paginator.get_page(page)
+    context_dict = {"movies" : movies}
     return render(request, 'censura/movies.html', context=context_dict)
 
 def view_movie(request, movie_name_slug):
