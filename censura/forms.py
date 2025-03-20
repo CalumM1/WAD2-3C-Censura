baedf8be9cdabed98837428a0b73150528e89659
Calum McCarthy <2004mccarthy@gmail.com>
2025-03-20 19:13:16 +0000

    add profile picture functionality

--- censura/forms.py
+++ censura/forms.py
@@ -19,7 +19,7 @@ class UserProfileForm(forms.ModelForm):
     
     class Meta:
         model = UserProfile
-        fields = ('likes',)
+        fields = ('likes', 'picture')
 
 
 class ReviewForm(forms.ModelForm):
